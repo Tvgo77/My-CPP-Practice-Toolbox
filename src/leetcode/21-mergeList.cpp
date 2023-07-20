32e23c7c73201c73e7cd480c8a6ea7b09af70f3e
Tvgo77 <tvgoshao77@gmail.com>
2023-07-20 07:44:09 +0000

    21-mergeList done

--- src/leetcode/21-mergeList.cpp
+++ src/leetcode/21-mergeList.cpp
@@ -9,11 +9,77 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+    /* Self-implement tool function */
+    void append(int x) {
+        ListNode* newNode = new ListNode(x);
+        ListNode* nextNode = this;
+        while (nextNode->next != nullptr) {
+            nextNode = nextNode->next;    
+        }
+        nextNode->next = newNode;
+    }
 };
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
+        ListNode* tail = new ListNode(0, nullptr);
+        ListNode* output = tail;
+
+        /* Loop, terminate when no uncompared element is left in list1 or list2*/
+        while (list1 != nullptr && list2 != nullptr) {
+            /* Get smallest element of list1 and list2 */
+            ListNode* smallerNode = (list1->val < list2->val)? list1: list2;
+            
+            /* Set address of tail-next to this element */
+            tail->next = smallerNode;
+
+            /* Update pointer*/
+            if (smallerNode == list1) {
+                list1 = list1->next;
+            }
+            else if (smallerNode == list2) {
+                list2 = list2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        while (list1 != nullptr) {
+            tail->next = list1;
+            list1 = list1->next;
+            tail = tail->next;
+        }
+
+        while (list2 != nullptr) {
+            tail->next = list2;
+            list2 = list2->next;
+            tail = tail->next;
+        }
+
+        return output->next;
     }
 };
+
+int main() {
+    Solution solution;
+    ListNode* input1 = new ListNode(1);
+    ListNode* input2 = new ListNode(2);
+    ListNode* expect = new ListNode(1);
+
+    input1->append(3);
+    input1->append(5);
+    input2->append(4);
+    input2->append(6);
+
+    expect->append(1);
+    expect->append(2);
+    expect->append(3);
+    expect->append(4);
+    expect->append(5);
+    expect->append(6);
+
+    testTool(&Solution::mergeTwoLists, expect, input1, input2);
+    return 0;
+}
